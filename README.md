fe55c6eaa6cfe8a60bdac8a81dc8f56220b01d30
axl <pommerpie@gmail.com>
2023-11-17 15:45:02 -0500

    Added README and added comments to code.

--- README.md
+++ README.md
@@ -1,1 +1,46 @@
-# SVG-Generator
+# SVG Generator
+
+  [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+## Table of contents
+
+[Description](#desc)
+
+[Installation](#install)
+
+[Usage](#usage)
+
+[License](#license)
+
+[Testing](#test)
+
+[Questions](#quest)
+
+[Demo](#demo)
+
+
+## Description
+
+    A command line prompt that will ask questions to generate an SVG logo.
+## Installation
+
+    Node.js, npm inquire, and npm jest
+## Usage
+
+    Inside the terminal.
+## License
+
+    MIT, https://opensource.org/licenses/MIT
+## Testing
+
+    Node index.js in the terminal, follow the prompts and wait and see if the SVG is rendered.
+## Questions
+
+    If you have questions about this project, please reach me here:
+
+    https://github.com/pommerpie
+    
+    pommerpie@gmail.com
+
+## Demo
+
+    https://drive.google.com/file/d/1pAocqJlgLnZFmBaNEgJ_Do4uQ6_lLFSr/view
--- index.js
+++ index.js
@@ -1,2 +1,62 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
+const { Triangle, Circle, Square } = require('./lib/shapes');
+
+const questions = [ // Questions
+    {
+        type: 'input',
+        message: 'Enter up to 3 characters.',
+        name: 'text',
+        validate: input => (input.length > 3) ? 'Please enter up to 3 characters.' : true
+    },
+    {
+        type: 'input',
+        message: 'Enter a text color.',
+        name: 'textColor'
+    },
+    {
+        type: 'list',
+        message: 'Choose a shape.',
+        choices: ['Triangle', 'Circle', 'Square'],
+        name: 'shape'
+    },
+    {
+        type: 'input',
+        message: 'Enter a shape color.',
+        name: 'shapeColor'
+    }
+]
+
+function makeSvg(text, textColor, shape, shapeColor){ // Grabs string for SVG
+    switch (shape) {
+        case 'Triangle':
+            const newTriangle = new Triangle(text, textColor, shapeColor) // Makes new Triangle object
+            return newTriangle.render() // Returns the SVG string
+            break;
+        case 'Circle':
+            const newCircle = new Circle(text, textColor, shapeColor) // Makes new Circle object
+            return newCircle.render() // Returns the SVG string
+            break;
+        case 'Square':
+            const newSquare = new Square(text, textColor, shapeColor) // Makes new Square object
+            return newSquare.render() // Returns the SVG string
+            break;
+    }
+}
+
+function start() {
+    inquirer.prompt(questions) // Renders questions
+        .then((answers) => {
+            const { text, textColor, shape, shapeColor } = answers // Separates answers
+            const svg = makeSvg(text, textColor, shape, shapeColor) // Renders SVG 
+            fs.writeFile('./examples/logo.svg', svg, (err) => { // Writes SVG file
+                if(err){
+                    console.log(err)
+                }
+                console.log('File created')
+            })
+        })
+}
+
+
+start() // Start function
